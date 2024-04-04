1f80765b9583a9d0fb2a124070f2e38142b83781
yafidaffaa <yafi.d.andriansyah@gmail.com>
2024-04-05 01:05:34 +0700

    commit all

---
+++ tugas7/script.js
@@ -0,0 +1,11 @@
+video.addEventListener("click", function Video() {
+    alert("Ini video Nancy Momoland");
+});
+
+audio.addEventListener("click", function audio() {
+    alert("Ini audio Despacito");
+});
+
+gambar.addEventListener("click", function gambar() {
+    alert("Ini gambar Girlband MOMOLAND");
+});
